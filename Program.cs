a3ece47ca383fcff4308b28f2fb51dfdb4400a81
Mohamed Mahmoud <mohamedmahmoudmido15@gmail.com>
2025-04-17 23:15:53 +0200

    Revert "Implement chat hub service"
    This reverts commit eea13b07ee4a20a6acdf71b4c934d43e744160d2.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Reunite.Data;
-using Reunite.Hubs;
 using Reunite.Repositories.Implementations;
 using Reunite.Repositories.Interfaces;
 using Reunite.Services.Implementations;
@@ -15,37 +14,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add controllers & API explorer
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-<<<<<<< Updated upstream
-=======
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ReuniteDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IChildRepository, ChildRepository>();
-builder.Services.AddScoped<IMessageRepository, MessageRepository>();
-builder.Services.AddScoped<IChatRepository, ChatRepository>();
-builder.Services.AddHttpClient<IChildService, ChildService>();
-builder.Services.AddScoped<IAuthService, AuthService>();
-
-var app = builder.Build();
-
-app.UseSwagger();
-app.UseSwaggerUI();
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-
-/*
->>>>>>> Stashed changes
 
 // Configure Auth0 JWT Authentication
 builder.Services.AddAuthentication(options =>
@@ -100,8 +68,6 @@ builder.Services.AddDbContext<ReuniteDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
-builder.Services.AddSignalR();
-
 // Repositories & Services
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IChildRepository, ChildRepository>();
@@ -128,7 +94,5 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
